b8e5c040617bf2acfc848ee7a61aeed9d61d9149
Mikko <mikko@optinetsolutions.com>
2026-02-16 18:07:56 +0800

    added profile ids for other countries and enabled them

--- src/config/countries.js
+++ src/config/countries.js
@@ -1,15 +1,15 @@
 export const CONFIG = {
   CANADA_PROFILEID: '697b1b609a01a2ed8d8792ca',
-  GERMANY_PROFILEID: 'TBC',
-  NEWZEALAND_PROFILEID: 'TBC',
-  AUSTRIA_PROFILEID: 'TBC',
-  NORWAY_PROFILEID: 'TBC'
+  GERMANY_PROFILEID: '6992b822449855d844be8782',
+  NEWZEALAND_PROFILEID: '6992bc124afa1a1f91e1423a',
+  AUSTRIA_PROFILEID: '6992bf57dcb37d00d4458c27',
+  NORWAY_PROFILEID: '6992dc7f35979e28c41b6179'
 };
 
 export const COUNTRIES = [
-  { name: 'Austria', value: CONFIG.AUSTRIA_PROFILEID, disabled: true },
+  { name: 'Austria', value: CONFIG.AUSTRIA_PROFILEID, disabled: false },
   { name: 'Canada', value: CONFIG.CANADA_PROFILEID, disabled: false },
-  { name: 'Germany', value: CONFIG.GERMANY_PROFILEID, disabled: true },
-  { name: 'New Zealand', value: CONFIG.NEWZEALAND_PROFILEID, disabled: true },
-  { name: 'Norway', value: CONFIG.NORWAY_PROFILEID, disabled: true }
+  { name: 'Germany', value: CONFIG.GERMANY_PROFILEID, disabled: false },
+  { name: 'New Zealand', value: CONFIG.NEWZEALAND_PROFILEID, disabled: false },
+  { name: 'Norway', value: CONFIG.NORWAY_PROFILEID, disabled: false }
 ];
